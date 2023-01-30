007210cad880948ce555c27b0b5da68d9f7c68db
Luas22 <103633140+Luas22@users.noreply.github.com>
2023-01-30 13:20:51 +0000

    Add files via upload

---
+++ MyProject01_23/js/indexScript.js
@@ -0,0 +1,27 @@
+function myFuncDiv1(){
+    var z = document.getElementById("MAINWORKDIV")
+    if(z.style.display ==="none"){
+        z.style.display ="block"
+    }else{
+        z.style.display="none"
+    }
+}
+
+function myFuncDiv2(){
+    var x = document.getElementById("MAINTOOLSDIV")
+    if(x.style.display ==="none"){
+        x.style.display ="block"
+    }else{
+        x.style.display="none"
+    }
+}
+
+function myFuncDiv3(){
+    var c = document.getElementById("aboutMEDiv")
+    if(c.style.display==="none"){
+        c.style.display="block"
+    }else{
+        c.style.display="none"
+    }
+}
+audioElement.play();
